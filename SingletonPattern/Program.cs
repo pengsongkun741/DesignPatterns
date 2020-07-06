97e580351ab9c0093c8b3e4ad7397600bca41d14
pengsongkun741@gmail.com <a5885516>
2020-07-06 20:28:00 +0800

    单例模式创建

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace DesignPatterns
+namespace SingletonPattern
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Hello World!");
         }
     }
 }
---
+++ SingletonPattern/lmpl/SingletonInstance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    internal class SingletonInstance
+    {
+        private static volatile SingletonInstance _instance;
+        private static readonly object _lock = new object();
+
+        private SingletonInstance()
+        {
+
+        }
+
+        public SingletonInstance GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonInstance();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
